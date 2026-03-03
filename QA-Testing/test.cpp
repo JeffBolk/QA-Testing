96be683c26eae8e90254bf41cf2a308f843644ab
JeffBolk <jeffreybolk05@gmail.com>
2026-03-03 09:54:41 -0500

    Add project files.

---
+++ QA-Testing/test.cpp
@@ -0,0 +1,12 @@
+#include "pch.h"
+#include "C:\cpaVsPrograms\Year2Sem2\QualityAssurance\QA-Testing\QA-Attendance\add.cpp"
+
+TEST(TestCaseName, TestName) {
+  EXPECT_EQ(4, 4);
+  EXPECT_TRUE(true);
+}
+
+TEST(TestCaseName, TestAdd) {
+	EXPECT_EQ(4, add(3,1));
+	EXPECT_TRUE(true);
+}
